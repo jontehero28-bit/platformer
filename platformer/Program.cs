7c344eb3361112ed1d3abcce3cc98eb59f135057
Yuriy Petrov <Jurijp2005@gmail.com>
2023-01-22 22:11:57 +0100

    commit2

--- platformer/Program.cs
+++ platformer/Program.cs
@@ -1,9 +1,15 @@
 ﻿using Raylib_cs;
 using System.Numerics;
 
-Raylib.InitWindow(800, 800, "Vinterprojekt");
+
+Raylib.InitWindow(1200, 900, "Vinterprojekt");
 Raylib.SetTargetFPS(60);
 
+Texture2D ninja = Raylib.LoadTexture("ninja.png");
+
+Rectangle player = new Rectangle(0, 820, ninja.width, ninja.height);
+
+float speed = 5f;
 string currentScene = "start"; //scenes start, game, end
 
 while(Raylib.WindowShouldClose() == false)
@@ -11,12 +17,31 @@ while(Raylib.WindowShouldClose() == false)
     //logik
     if(currentScene == "game")
     {
+        if (Raylib.IsKeyDown(KeyboardKey.KEY_D)  || Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT))
+        {
+            player.x += speed * 2;
+        }
+        else if (Raylib.IsKeyDown(KeyboardKey.KEY_A) || Raylib.IsKeyDown(KeyboardKey.KEY_LEFT))
+        {
+            player.x -= speed * 2;
+        }
 
     }
 
     else if (currentScene == "start")
     {
+        if (Raylib.IsKeyPressed(KeyboardKey.KEY_ENTER))
+        {
+            currentScene = "game";
+        }
+    }
 
+    else if (currentScene == "end")
+    {
+        if (Raylib.IsKeyPressed(KeyboardKey.KEY_ENTER))
+        {
+            currentScene = "game";
+        }
     }
 
     //grafik
@@ -26,19 +51,19 @@ while(Raylib.WindowShouldClose() == false)
 
     if (currentScene == "game")
     {
-
+        Raylib.DrawTexture(ninja, (int)player.x, (int)player.y, Color.WHITE);
     }
 
     else if (currentScene == "start")
     {
+        Raylib.DrawText("Welcome to Dumb Ninja. In this game you play as a dumb Ninja", 100, 200, 32, Color.BLACK);
         Raylib.DrawText("Press ENTER to start", 250, 370, 32, Color.BLACK);
     }
     else if (currentScene == "end")
     {
-        Raylib.DrawText("GAME OVER", 350, 370, 32, Color.BLACK);
+        Raylib.DrawText("GAME OVER", 350, 300, 32, Color.BLACK);
+        Raylib.DrawText("Press ENTER to play again", 300, 400, 32, Color.BLACK);
     }
 
     Raylib.EndDrawing();
 }
-
-Console.WriteLine("Hello, World!");
